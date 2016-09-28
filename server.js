81ee5d7d73a6e84525457da2949cf0cf1c42fc14
Pranshup77 <pranshup77@gmail.com>
2016-09-29 00:04:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,58 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title:'Article One | Pranshu Pathak',
+    heading:'Article One',
+    date:'Sep 5, 2016',
+    content:`
+                <p>This is my first Article Content.This is my first Article Content.This is my first Article Content.This is my first Article Content.This is my first Article Content.
+                </p>
+                <p>This is my first Article Content.This is my first Article Content.This is my first Article Content.This is my first Article Content.This is my first Article Content.
+                </p>
+                <p>This is my first Article Content.This is my first Article Content.This is my first Article Content.This is my first Article Content.This is my first Article Content.
+                </p>
+    `
+    };
+function createTemplate(data){
+    
+    var title = data.title;
+    var date = data.date;
+    var heading =data.heading;
+    var content = data.content;
+
+var htmltemplate = `
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+       <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+            <div>
+            <a href="/">Home</a>
+            </div>
+            <hr/>
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+                ${content}
+            </div>
+        </div>
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -18,7 +70,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 
 
