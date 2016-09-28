a7a6473001a8200109b704884cfdcd3ab634bc8c
Pranshup77 <pranshup77@gmail.com>
2016-09-29 00:45:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,6 +91,7 @@ app.get('/ui/madi.png', function (req, res) {
 app.get('/:articleName',function(req,res){
     //articalName == article-one
     //articles[articalName] == {} content object for article one
+    var articleName = req.params.articleName;
     res.send(createTemplate(articles[articalName]));
 });
 
