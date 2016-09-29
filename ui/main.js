9b5acb5c404b0acaec582620ee113507e0de63df
Pranshup77 <pranshup77@gmail.com>
2016-09-29 11:24:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,14 @@
 console.log('Loaded!');
+
 //change the text of the main-text file
 var element = document.getElementById('main-text');
+
 element.innerHTML = 'New value';
+
 //move the image
 var img = document.getElementById('madi');
 img.onClick = function(){
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveLeft,100);
+    //1st line then above line 
+    //img.style.marginLeft = '100px';
 };
