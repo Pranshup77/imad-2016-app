d26fd51b6b55b9c6fac494711e858b8ac1e5ad2a
Pranshup77 <pranshup77@gmail.com>
2016-09-29 11:09:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 //move the image
-var element = document.getElementById('img');
-img.onClick = function(){
-    img.style.marginLeft = '100px';
+var element = document.getElementById('madi');
+madi.onClick = function(){
+    madi.style.marginLeft = '100px';
 };
