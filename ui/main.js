0f80ec76d75ac71b973b46c6557cf924583aa58a
Pranshup77 <pranshup77@gmail.com>
2016-09-29 11:27:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,11 @@ element.innerHTML = 'New value';
 
 //move the image
 var img = document.getElementById('madi');
+var margingLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft+10;
+    img.style.marginLeft + 'px';
+}
 img.onClick = function(){
     var interval = setInterval(moveReft,100);
     //1st line then above line 
